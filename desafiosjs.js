e1b404e1a2cbcc16475cb92733d67f867b9c6530
Sean Patrick Tchieizer <143755628+SeanPTB@users.noreply.github.com>
2024-02-24 21:21:06 -0300

    Feat: Criação Jogo

---
+++ desafiosjs.js
@@ -0,0 +1,81 @@
+// desafiosjs.js
+
+let currentLevel = 1;
+let score = 20;
+let hintUsed = false;
+
+const levelHints = {
+    1: 'SMS',
+    2: '7',
+    3: '7 de setembro de 1906',
+    // Adicione mais dicas conforme necessário para outros níveis
+};
+
+function checkAnswer(level) {
+    const userInput = document.getElementById(`userInput${level}`).value.toLowerCase();
+    const correctAnswers = {
+        1: 'desafio',
+        2: 'arco iris',
+        3: 'alberto santos dumont',
+        // Adicione mais respostas conforme necessário
+    };
+
+    if (userInput === correctAnswers[level]) {
+        score += 30;
+        alert('Parabéns! Você acertou!');
+        document.querySelector(`#level${level}`).style.display = 'none';
+        goToNextLevel();
+    } else {
+        score = Math.max(0, score - 60);
+
+        if (score <= 0) {
+            alert('Você Perdeu! Tente Novamente :)');
+            window.location.href = 'index.html';
+        } else {
+            alert('Resposta incorreta. Tente novamente!');
+        }
+    }
+
+    document.querySelector('.score').textContent = `Pontuação: ${score}`;
+}
+
+function goToNextLevel() {
+    currentLevel++;
+
+    const nextLevelElement = document.querySelector(`#level${currentLevel}`);
+
+    if (nextLevelElement) {
+        document.getElementById('levelCounter').textContent = `Nível ${currentLevel}`;
+        nextLevelElement.style.display = 'block';
+        document.getElementById(`userInput${currentLevel}`).value = '';
+    } else {
+        alert('Parabéns! Você concluiu todos os níveis.');
+        window.location.href = 'index.html';
+    }
+}
+
+function showHint() {
+    if (score >= 20 && !hintUsed) {
+        const hint = levelHints[currentLevel];
+
+        if (hint !== undefined) {
+            alert(`Aqui está a dica para o Nível ${currentLevel}: ${hint}`);
+            score -= 20;
+            document.querySelector('.score').textContent = `Pontuação: ${score}`;
+            hintUsed = true;
+        } else {
+            alert('Nenhuma dica disponível para este nível.');
+        }
+    } else if (hintUsed) {
+        alert('Você já comprou a dica para este nível.');
+    } else {
+        alert('Você não tem saldo suficiente para dicas!');
+    }
+}
+
+
+
+function closeHint() {
+    document.getElementById('hintPopup').style.display = 'none';
+}
+
---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('playButton').addEventListener('click', function() {
+    // Redirecionar para a página de desafios ao clicar no botão PLAY
+    window.location.href = 'desafios.html';
+});
+
+document.getElementById('howToPlayButton').addEventListener('click', function() {
+    // Lógica para exibir as orientações do jogo ao clicar no botão "Como Jogar"
+    var instructions = "Este é um jogo simples, descubra os enigmas e ganhe pontos para avançar. Cada acerto vale 30 pontos e cada erro custa 60 pontos. Você tem direito a uma única dica por nível com o custo de 20 pontos que pode ser adiquirida no icone inferior direito '?'. Caso você chegue a 0 pontos, você perdeu! Para as respostas são consideradas apenas letras minúsculas sem caracteres especiais ou acentos. Alguns níveis contêm como resposta números.";
+    alert(instructions);
+});
+
+document.getElementById('contactButton').addEventListener('click', function() {
+    // Lógica para redirecionar para a página do Instagram ao clicar no botão "Fale Conosco"
+    window.location.href = 'https://www.instagram.com/sean_ptb/';
+});

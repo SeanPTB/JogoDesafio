3808b09764efdb21d2fba919768325f692b85fd2
Sean Patrick Tchieizer <143755628+SeanPTB@users.noreply.github.com>
2024-02-25 08:49:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('playButton').addEventListener('click', function() {
 
 document.getElementById('howToPlayButton').addEventListener('click', function() {
     // Lógica para exibir as orientações do jogo ao clicar no botão "Como Jogar"
-    var instructions = "Este é um jogo simples, descubra os enigmas e ganhe pontos para avançar. Cada acerto vale 30 pontos e cada erro custa 60 pontos. Você tem direito a uma única dica por nível com o custo de 20 pontos que pode ser adiquirida no icone inferior direito '?'. Caso você chegue a 0 pontos, você perdeu! Para as respostas são consideradas apenas letras minúsculas sem caracteres especiais ou acentos. Alguns níveis contêm como resposta números.";
+    var instructions = "Este é um jogo simples, descubra os enigmas e ganhe pontos para avançar. Cada acerto vale 30 pontos e cada erro custa 30 pontos. Você tem direito a uma única dica por nível com o custo de 20 pontos que pode ser adiquirida no icone inferior direito '?'. Caso você chegue a 0 pontos, você perdeu! Para as respostas são consideradas apenas letras minúsculas sem caracteres especiais ou acentos. Alguns níveis contêm como resposta números.";
     alert(instructions);
 });
 
